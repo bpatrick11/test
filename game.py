a1c1ba27e04f7be640a51a2e9f6f23e11e796979
bpatrick11 <bryce_patrick@comcast.net>
2026-04-22 18:54:26 -0400

    Update game.py

--- game.py
+++ game.py
@@ -20,10 +20,39 @@ class Producer:
 
         return currency
 
+class Drain:
+    def __init__(self, timeM, debuff):
+        self.timeS = timeM * 60
+        self.nextDrainTimerSec = self.timeS
+        self.drainActive = False
+        self.drainDebuff = debuff  # e.g. 0.5 = 50%
+
+    def stop_drain(self):
+        self.drainActive = False
+        self.nextDrainTimerSec = self.timeS
+
+    def drain_increment_sec(self):
+        if self.drainActive:
+            return  # do nothing while active
+
+        self.nextDrainTimerSec -= 1
+
+        if self.nextDrainTimerSec <= 0:
+            self.drainActive = True
+
+    def get_multiplier(self):
+        if self.drainActive:
+            return self.drainDebuff
+        return 1
+
 
 # --- GAME STATE ---
-currency = 0
-currencypersecond = 1
+currency = 0 # total currency
+currencypersecond = 1 # currency added tot total per second
+minigameactive = False # if the drain minigame is active
+# should the minigame var be here or in the drain class??
+
+drain = Drain(0.25, 0.5)  # activates every 15s, cuts CPS in half
 
 # Create 5 producers
 producers = [
@@ -45,7 +74,12 @@ def increment():
 
 def second():
     global currency
-    currency += currencypersecond
+
+    drain.drain_increment_sec()
+
+    multiplier = drain.get_multiplier()
+    currency += currencypersecond * multiplier
+
     return currency
 
 def CPS():
@@ -59,4 +93,13 @@ def get_price(index):
 
 def get_owned(index):
     return producers[index].owned
+    
+def get_production(index):
+    return producers[index].production
 
+def is_drain_active():
+    return drain.drainActive
+
+def stop_drain():
+    drain.stop_drain()
+    return currency
